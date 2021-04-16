a98895ca73b4658d70f42e07398b47c48814ee7b
Aman Subhan <amansubhan929@gmail.com>
2021-04-16 22:42:57 +0500

    updated ws server for deployment testing

--- botme-ws-server/src/app.js
+++ botme-ws-server/src/app.js
@@ -1,12 +1,13 @@
 // requires for libraries
 const WebSocket = require('ws');
 const http = require('http');
-const dbUtil = require('./utils/dbUtil');
-const rm = require('./utils/queue');
+//const dbUtil = require('./utils/dbUtil');
+//const queue = require('./utils/queue');
+const Request = require("./models/request");
 
 // application constants
 const port = process.env.WS_PORT || 6380;
-const apiServer = process.env.CLIENTS_API || 'http://localhost:3000/client/'
+//const apiServer = process.env.CLIENTS_API || 'http://localhost:3000/client/'
 const bearerToken = 'ea2d3aeaad77865f9769974a920892f5'
 var sessions = {}
 
@@ -16,40 +17,50 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
 
-    ws.on('message', function incoming(response) {
-        console.log(response);
-        message = JSON.parse(response);
+    ws.on('message', function incoming(payload) {
 
-        if (message.hasOwnProperty('authToken')) {
-            dbUtil.getSession(message.authToken, function (session) {
-                if (session == null) {
-                    ws.send('HTTP/1.1 401 Unauthorized\r\n\r\n');
-                    ws.close();
-                    return;
-                }
-                console.log(session.clientID);
-                ws.clientID = session.clientID;
-                sessions[ws.clientID] = ws
-                ws.send(JSON.stringify({ "clientID": session.clientID }));
-            });
+        let data = new Request(payload)
+
+        if (data.authToken) {
+            // FIXME: This is functional code commenting for testing
+
+            // dbUtil.getSession(data.authToken, function (session) {
+            //     if (session == null) {
+            //         ws.send('HTTP/1.1 401 Unauthorized\r\n\r\n');
+            //         ws.close();
+            //         return;
+            //     }
+            //     console.log(session.clientID);
+            //     ws.clientID = session.clientID;
+            //     sessions[ws.clientID] = ws
+            //     ws.send(JSON.stringify({ "clientID": session.clientID }));
+            // });
+            if (data.authToken === bearerToken) {
+                ws.send(Date() + " | Message received on server: " + data.message_text)
+            }
+
+
         } else {
-            console.log(message);
+            ws.send('authToken not found')
         }
     });
 
     // TODO: set interval timeout to 0 when deploying
-    setInterval(async function () {
-        var recMessage = await rm.receiveMessage();
-        if (recMessage.clientID) {
 
-            var clientID = recMessage.clientID;
-            var command = recMessage.command;
-            if (sessions.hasOwnProperty(clientID))
-                sessions[clientID].send(JSON.stringify({ "clientID": clientID, "command": command }));
+    // FIXME: This is functional code commenting for testing
+    // setInterval(async function () {
+    //     // receive message from SQS
+    //     var recMessage = await queue.receiveMessage();
+    //     if (recMessage.clientID) {
 
-            rm.deleteMessage(recMessage.ReceiptHandle);
-        }
-    }, 5000);
+    //         var clientID = recMessage.clientID;
+    //         var command = recMessage.command;
+    //         if (sessions.hasOwnProperty(clientID))
+    //             sessions[clientID].send(JSON.stringify({ "clientID": clientID, "command": command }));
+
+    //         queue.deleteMessage(recMessage.ReceiptHandle);
+    //     }
+    // }, 50000);
 
 });
 
--- botme-ws-server/src/models/request.js
+++ botme-ws-server/src/models/request.js
@@ -1,14 +1,17 @@
 class Request {
     constructor(payload) {
-
-        payload = JSON.parse(payload);
-
-        this.clientID = payload.clientID,
-            this.current_time = payload.current_time,
-            this.message_format = payload.message_format,
-            this.message_command = payload.message_command,
-            this.language = payload.language,
-            this.message_text = payload.message_text
+        try {
+            payload = JSON.parse(payload);
+            this.clientID = payload.clientID,
+                this.current_time = payload.current_time,
+                this.message_format = payload.message_format,
+                this.message_command = payload.message_command,
+                this.language = payload.language,
+                this.message_text = payload.message_text,
+                this.authToken = payload.authToken
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
 
