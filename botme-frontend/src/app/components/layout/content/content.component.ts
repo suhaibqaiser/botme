eb329c019c92b471a538b1bc8badc095b3fb7f5d
Suhaib Qaiser <suhaibqaiser@yahoo.com>
2021-05-08 14:33:08 -0400

    fixing some issues

--- botme-frontend/src/app/components/layout/content/content.component.ts
+++ botme-frontend/src/app/components/layout/content/content.component.ts
@@ -11,13 +11,13 @@ import {Observable} from "rxjs";
 })
 export class ContentComponent implements OnInit {
 
-  pageTitle: string;
-
   constructor(private router: Router, private actRouter: ActivatedRoute) {
-    this.pageTitle = this.router.routerState.snapshot.url.split('/')[1].toUpperCase()
   }
 
   ngOnInit(): void {
+  }
 
+  getPageHeader(): String {
+    return this.router.routerState.snapshot.url.split('/')[1].toUpperCase();
   }
 }
---
+++ botme-frontend/src/app/services/header.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Router} from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+  private pageHeader:String = "";
+
+  public getHeader() {
+    return this.pageHeader;
+  }
+  public setHeader(value:String) {
+    this.pageHeader = value;
+  }
+}
