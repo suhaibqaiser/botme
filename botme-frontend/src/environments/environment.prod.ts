a31c6c729a6dfa8c9948fb69592faa7f27229605
Aman Subhan <amansubhan929@gmail.com>
2021-05-17 23:46:08 +0500

    updating prod api url

--- botme-frontend/src/environments/environment.prod.ts
+++ botme-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "http://localhost:3000",
+  apiBaseUrl: "http://ec2-54-172-57-254.compute-1.amazonaws.com:3000",
   BearerToken: "ea2d3aeaad77865f9769974a920892f5"
 };
