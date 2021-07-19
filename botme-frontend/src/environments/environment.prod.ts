542cbb53265452357da6c33730d32df84931a822
Aman Subhan <amansubhan929@gmail.com>
2021-07-19 19:12:00 +0500

    Updated api urls for https deployment

--- botme-frontend/src/environments/environment.prod.ts
+++ botme-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://api.gofindmenu.com",
-  apiRestaurantUrl: "https://api.gofindmenu.com:3100",
+  apiBaseUrl: "https://api.gofindmenu.com/client",
+  apiRestaurantUrl: "https://api.gofindmenu.com/restaurant",
   BearerToken: "ea2d3aeaad77865f9769974a920892f5"
 };
