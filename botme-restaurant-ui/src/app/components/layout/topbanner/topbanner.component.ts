6d333f069096249e424c25d61c9e286ecc5292d2
mohsin-d4interactive <mohsin.ali@d4interactive.io>
2021-09-28 23:02:27 +0500

    design updated of home page

--- botme-restaurant-ui/src/app/components/layout/topbanner/topbanner.component.ts
+++ botme-restaurant-ui/src/app/components/layout/topbanner/topbanner.component.ts
@@ -10,7 +10,29 @@ export class TopbannerComponent implements OnInit {
 
   categories: any = []
   products: any = []
-  corusalProductList: any = []
+  corusalProductList: any = [
+    {
+      productId: '4731852b-aabb-42b5-99cf-22447903f79e',
+      productName: 'Nacho Burger',
+      productDescription: 'A simple burger with lettuce and nachos with single patty',
+      productPrice: 5.99,
+      productImage: ['Nacho Burger.jpg']
+    },
+    {
+      productId: '8a676882-6173-49b4-b123-866899b796eb',
+      productName: 'Falafel Sandwich',
+      productDescription: '6 pieces of falafel served with lettuce, tomato, pickle, turnip, parsl...',
+      productPrice: 12.99,
+      productImage: ['Falafel Sandwich.jpg']
+    },
+    {
+      productId: 'ba0b7491-3431-4884-bfae-b0f6faef7cda',
+      productName: 'Chicken Tenders',
+      productDescription: '2 pieces of chicken tenders',
+      productPrice: 7.99,
+      productImage: ['Chicken Tenders.jpg']
+    }
+  ]
 
   constructor(private menuService: MenuService) {
   }
@@ -21,16 +43,23 @@ export class TopbannerComponent implements OnInit {
 
   async getCategory() {
     this.menuService.getCategory()
-      .subscribe(result => {
+      .subscribe(async result => {
         this.categories = result.payload
         console.log('categories', result.payload)
         if (Array.isArray(this.categories)) {
-          this.getProducts();
+          await this.getProducts();
         } else {
           this.categories = []
         }
         return
       });
+  }
+
+  resolveImages(product: any) {
+    if (product.productImage && product.productImage.length) {
+      return 'assets/images/products/' + product.productImage[0]
+    }
+    return 'assets/images/product-1.png'
   }
 
   async getProducts() {
