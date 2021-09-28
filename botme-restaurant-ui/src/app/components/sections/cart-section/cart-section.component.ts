835f3e781dc853666ab3f96db6ffb4fe0e3dec91
mohsin-d4interactive <mohsin.ali@d4interactive.io>
2021-09-29 00:45:53 +0500

    cart modal added

--- botme-restaurant-ui/src/app/components/sections/cart-section/cart-section.component.ts
+++ botme-restaurant-ui/src/app/components/sections/cart-section/cart-section.component.ts
@@ -25,14 +25,13 @@ export class CartSectionComponent implements OnInit {
   getCartProducts() {
     this.cartService.getCartProducts().subscribe(
       res => {
-        this.productIds = JSON.parse(res)
-        this.cartProducts = []
+        // this.productIds = JSON.parse(res)
+        this.cartProducts = JSON.parse(res)
         this.cartTotal = 0
-        for (let id in this.productIds) {
-          this.cartProducts.push(this.products.find((product: { productId: string }) => product.productId === this.productIds[id]));
-          this.cartTotal += this.cartProducts[id].productRate.standard
-        }
-        console.log(this.cartProducts)
+        // for (let id in this.productIds) {
+        //   this.cartProducts.push(this.products.find((product: { productId: string }) => product.productId === this.productIds[id]));
+        //   this.cartTotal += this.cartProducts[id].productRate.standard
+        // }
       }
     )
   }
@@ -45,8 +44,75 @@ export class CartSectionComponent implements OnInit {
       });
   }
 
+  calculateTotalPrice(productToppings: any) {
+    let price = 0
+    if (productToppings && productToppings.length) {
+      productToppings.forEach((item: any) => {
+        price += item.productTotalPrice
+      })
+    }
+    return price
+  }
+
+  totalCartPrice() {
+    let price = 0
+    if (this.cartProducts && this.cartProducts.length) {
+      this.cartProducts.forEach((item: any) => {
+        price += item.productTotalPrice
+      })
+    }
+    return price
+  }
+
   removeFromCart(productId: string) {
     this.cartService.removeFromCart(productId);
+  }
+
+  resolveImages(product: any) {
+    if (product.productImage && product.productImage.length) {
+      return 'assets/images/products/' + product.productImage[0]
+    }
+    return 'assets/images/product-1.png'
+  }
+
+  customizeBillCalculation(product: any) {
+    product.productTotalPrice = product.productPrice
+    product.productToppings.forEach((item: any) => {
+      product.productTotalPrice += Math.ceil(item.productTotalPrice)
+    })
+
+    product.productAddons.forEach((item: any) => {
+      product.productTotalPrice += item.productTotalPrice
+    })
+  }
+
+  addProductQuantity(product: any, type: any) {
+    this.customizeBillCalculation(product)
+    if (type === 'adding') {
+      product.productQuantity = product.productQuantity + 1
+    } else if (type === 'subtracting') {
+      if (product.productQuantity === 1) return
+      product.productQuantity = product.productQuantity - 1
+    }
+    product.productTotalPrice = product.productTotalPrice * product.productQuantity
+    this.editFromCart(product)
+  }
+
+  getSubTotal() {
+    let total = 0
+    if (this.cartProducts && this.cartProducts.length) {
+      this.cartProducts.forEach((item: any) => {
+        total += item.productTotalPrice
+      })
+    }
+    return total
+  }
+
+  editFromCart(product: any) {
+    // document.getElementsByClassName('cart-modal-wrapper')[0].setAttribute('style','display:none')
+    this.cartService.singleCustomProductObj = JSON.parse(JSON.stringify(product))
+    this.cartService.singleCustomProductObj.isEditable = true
+    this.cartService.selectProductRatesField.setValue(product.productServingSize)
   }
 
 }
