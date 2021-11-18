145751582f4b9098da0bdc5d704e482864fc2a46
mohsin-d4interactive <mohsin.ali@d4interactive.io>
2021-11-18 23:47:38 +0500

    form added

--- botme-restaurant-ui/src/app/components/sections/booking-section/booking-section.component.ts
+++ botme-restaurant-ui/src/app/components/sections/booking-section/booking-section.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-booking-section',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking-section.component.scss']
 })
 export class BookingSectionComponent implements OnInit {
+  reservationForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    number_of_person: new FormControl('', Validators.required),
+    date: new FormControl('', Validators.required),
+    time: new FormControl('', Validators.required),
+  })
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  addReservation() {
+    console.log('reservation form =>', this.reservationForm.value)
+  }
 }
