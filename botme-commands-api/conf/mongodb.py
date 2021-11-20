ccc017e7824a85fea07e803748a8311f72c23eb5
Taha-Hasan <88339456+Taha-Hasan@users.noreply.github.com>
2021-11-20 11:42:41 +0500

    command api and nlu updated

--- botme-commands-api/conf/mongodb.py
+++ botme-commands-api/conf/mongodb.py
@@ -2,26 +2,35 @@ from flask_pymongo import MongoClient
 from config import MONGO_URL
 
 def getDbCta(intent,entity,pageId,sectionId):
-
     mydb = MongoClient(MONGO_URL)
-    # print(mydb.list_database_names())
     db = mydb['food']
     mycollection = db['ctas']
-    if (entity == None):
+    if (entity != None):
+        if (intent == "unreserved_table_person" or intent == "inform_name"):
+            my_query = {"intentName":intent,"context.pageId":pageId,"context.sectionId":sectionId}
+            mycta = mycollection.find(my_query)
+            for x in mycta:
+                print(x)
+                if (len(x) == 0):
+                    return None
+                else:
+                    return x
+        else:
+            value = entity.title()
+            my_query = {"intentName":intent,"context.entities.entityName":value,"context.pageId":pageId,"context.sectionId":sectionId}
+            mycta = mycollection.find(my_query)
+            for x in mycta:
+                if(len(x) == 0):
+                    return None
+                else:
+                    return x
+
+    else:
         my_query = {"intentName":intent,"context.pageId":pageId,"context.sectionId":sectionId}
         mycta = mycollection.find(my_query)
         for x in mycta:
-            print(x)
             if (len(x) == 0):
                 return None
             else:
                 return x
-    else:
-        value = entity.title()
-        my_query = {"intentName":intent,"context.entities.entityName":value,"context.pageId":pageId,"context.sectionId":sectionId}
-        mycta = mycollection.find(my_query)
-        for x in mycta:
-            if(len(x) == 0):
-                return None
-            else:
-                return x
+        
