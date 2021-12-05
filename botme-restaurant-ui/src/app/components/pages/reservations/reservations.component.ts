272d4c06bfd65b7d6dcf90038a15920c82bbadc8
mohsin-d4interactive <mohsin.ali@d4interactive.io>
2021-12-05 20:02:02 +0500

    scroll issue fixed

--- botme-restaurant-ui/src/app/components/pages/reservations/reservations.component.ts
+++ botme-restaurant-ui/src/app/components/pages/reservations/reservations.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ReservationsComponent implements OnInit {
 
   constructor() {
-    window.scrollTo(0,2000)
+    window.scrollTo(0,1000)
   }
 
   ngOnInit(): void {
