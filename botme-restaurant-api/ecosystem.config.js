fe6df5891cf4c2a0204b38f90a1808c67bcb8af9
Aman Subhan <amansubhan929@gmail.com>
2022-01-04 17:01:09 +0500

    Updated startup configs and dependencies

--- botme-restaurant-api/ecosystem.config.js
+++ botme-restaurant-api/ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  apps : [
-      {
-        name: "botme-restaurant-api",
-        script: "dist/server.js",
-        watch: true,
-        env: {
-            "PORT": 3100,
-            "NODE_ENV": "development"
-        },
-        env_production: {
-            "PORT": 3100,
-            "NODE_ENV": "production",
-        }
+  apps: [
+    {
+      name: "botme-restaurant-api",
+      script: "dist/server.js",
+      watch: false,
+      env: {
+        "PORT": 3100,
+        "NODE_ENV": "development"
+      },
+      env_production: {
+        "PORT": 3100,
+        "NODE_ENV": "production",
       }
+    }
   ]
 }
 
