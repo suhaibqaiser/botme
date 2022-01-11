30b297f0833d76c4f2fdd3bf4f964462452f1edb
mohsin-d4interactive <mohsin.ali@d4interactive.io>
2022-01-12 01:10:09 +0500

    context issue fixe

--- botme-restaurant-ui/src/app/services/cart.service.ts
+++ botme-restaurant-ui/src/app/services/cart.service.ts
@@ -404,12 +404,12 @@ export class CartService {
 
   previousSlide() {
     this.slideToShow--
-    this.setCurrentContext()
     if (this.slideToShow === 3 && !this.singleCustomProductObj.productIngredients.length && !this.singleCustomProductObj.productFlavors.length) {
       this.slideToShow = 0
     } else if (this.slideToShow === 2 && !this.singleCustomProductObj.productToppings.length) {
       this.slideToShow--
     }
+    this.setCurrentContext()
   }
 
   nextSlide() {
