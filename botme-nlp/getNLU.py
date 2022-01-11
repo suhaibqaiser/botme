aceecbb1468d874682aed841f9a3202e0c27fed1
Aman Subhan <amansubhan929@gmail.com>
2022-01-12 00:23:59 +0500

    Added corpusId into NLU download utility

--- botme-nlp/getNLU.py
+++ botme-nlp/getNLU.py
@@ -1,14 +1,20 @@
 from requests.structures import CaseInsensitiveDict
 import requests
+import sys
 
-url = "http://localhost:3000/nlp/export"
 
-headers = CaseInsensitiveDict()
-headers["Accept"] = "application/json"
-headers["Authorization"] = "Bearer ea2d3aeaad77865f9769974a920892f5"
+def main():
+    corpusId = sys.argv[1]
+    print(corpusId)
+    url = "http://localhost:3000/nlp/export?corpusId=" + corpusId
+    headers = CaseInsensitiveDict()
+    headers["Accept"] = "application/json"
+    headers["Authorization"] = "Bearer ea2d3aeaad77865f9769974a920892f5"
+    resp = requests.get(url, headers=headers).json()
+    nlu = open("data/nlu.yml", "w")
+    nlu.writelines(resp['payload'])
+    nlu.close()
 
-resp = requests.get(url, headers=headers).json()
 
-nlu = open("data/nlu.yml", "w")
-nlu.writelines(resp['payload'])
-nlu.close()
+if __name__ == "__main__":
+    main()
