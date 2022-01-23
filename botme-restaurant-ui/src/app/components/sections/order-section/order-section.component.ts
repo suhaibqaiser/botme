dd272b88896738193bedf259dedc9a1ccde6eac3
mohsin-d4interactive <mohsin.ali@d4interactive.io>
2022-01-23 20:44:16 +0500

    dine in updated

--- botme-restaurant-ui/src/app/components/sections/order-section/order-section.component.ts
+++ botme-restaurant-ui/src/app/components/sections/order-section/order-section.component.ts
@@ -11,6 +11,7 @@ import {BotmeClientService} from "../../../services/botme-client.service";
 export class OrderSectionComponent implements OnInit {
 
   constructor(public _orderService: OrderService, private _route: Router, private _botMeService: BotmeClientService) {
+    this._orderService.selectedOrderButtons[this._botMeService.getCookie()?.orderType] = true
   }
 
 
@@ -18,13 +19,35 @@ export class OrderSectionComponent implements OnInit {
   }
 
   selectOrderType(type: any) {
+
+   if(Object.keys(this._orderService.selectedOrderButtons).some(k => this._orderService.selectedOrderButtons[k])){
+
+     return
+   }
+
     if (type === 'dine_in') {
       this._orderService.selectedOrderButtons['dine_in'] = true
+      this._orderService.selectedOrderButtons['pick_up'] = false
+      this._orderService.selectedOrderButtons['delivery'] = false
       this._botMeService.setCookie('orderType','dine_in')
       return this._route.navigate(['reservations'])
     }
-    if (type === 'pick_up') return this._route.navigate(['online-shop'])
-    if (type === 'delivery') return this._route.navigate(['online-shop'])
+    if (type === 'pick_up') {
+      this._route.navigate(['online-shop'])
+      this._orderService.selectedOrderButtons['dine_in'] = false
+      this._orderService.selectedOrderButtons['pick_up'] = true
+      this._orderService.selectedOrderButtons['delivery'] = false
+      this._botMeService.setCookie('orderType','dine_in')
+      return
+    }
+    if (type === 'delivery') {
+      this._orderService.selectedOrderButtons['dine_in'] = false
+      this._orderService.selectedOrderButtons['pick_up'] = false
+      this._orderService.selectedOrderButtons['delivery'] = true
+      this._botMeService.setCookie('orderType','delivery')
+      this._route.navigate(['online-shop'])
+      return
+    }
     return ''
   }
 }
--- botme-restaurant-ui/src/app/services/order.service.ts
+++ botme-restaurant-ui/src/app/services/order.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OrderService {
-  selectedOrderButtons = {
+  selectedOrderButtons:any = {
     'dine_in': false,
     'pick_up': false,
     'delivery': false
