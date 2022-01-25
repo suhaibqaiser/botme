86463b2d6c4907f518b194e4f86e0db30402e050
mohsin-d4interactive <mohsin.ali@d4interactive.io>
2022-01-26 00:23:42 +0500

    modal added

--- botme-restaurant-ui/src/app/components/sections/order-section/order-section.component.ts
+++ botme-restaurant-ui/src/app/components/sections/order-section/order-section.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {OrderService} from "../../../services/order.service";
 import {BotmeClientService} from "../../../services/botme-client.service";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-order-section',
@@ -10,13 +11,26 @@ import {BotmeClientService} from "../../../services/botme-client.service";
 })
 export class OrderSectionComponent implements OnInit {
 
-  constructor(public _orderService: OrderService, private _route: Router, private _botMeService: BotmeClientService) {
+  constructor(public _orderService: OrderService, private _route: Router, public _botMeService: BotmeClientService) {
 
   }
 
+  orderId = new FormControl('')
 
   ngOnInit(): void {
     this._orderService.selectedOrderButtons[this._botMeService.getCookie().orderType] = true
+  }
+
+  getOrderType(){
+    return this._botMeService.getCookie().orderType.replace(/_/g, " ")
+  }
+
+  getStatement(){
+    return 'Do you have  ' +(['pick_up','delivery'].includes(this._botMeService.getCookie().orderType) ? 'Order' : 'Reservation') + ' Id ?'
+  }
+
+  getButtonName(){
+    return 'Apply ' +(['pick_up','delivery'].includes(this._botMeService.getCookie().orderType) ? 'Order' : 'Reservation') + ' Id'
   }
 
   selectOrderType(type: any) {
@@ -25,23 +39,18 @@ export class OrderSectionComponent implements OnInit {
       this._orderService.selectedOrderButtons['pick_up'] = false
       this._orderService.selectedOrderButtons['delivery'] = false
       this._botMeService.setCookie('orderType','dine_in')
-      return this._route.navigate(['reservations'])
     }
     if (type === 'pick_up') {
-      this._route.navigate(['online-shop'])
       this._orderService.selectedOrderButtons['dine_in'] = false
       this._orderService.selectedOrderButtons['pick_up'] = true
       this._orderService.selectedOrderButtons['delivery'] = false
       this._botMeService.setCookie('orderType','pick_up')
-      return
     }
     if (type === 'delivery') {
       this._orderService.selectedOrderButtons['dine_in'] = false
       this._orderService.selectedOrderButtons['pick_up'] = false
       this._orderService.selectedOrderButtons['delivery'] = true
       this._botMeService.setCookie('orderType','delivery')
-      this._route.navigate(['online-shop'])
-      return
     }
     return ''
   }
