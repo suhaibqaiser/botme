d6e71daf2442aa6a902b9e1fe6ff1c3536f59330
Taha-Hasan <88339456+Taha-Hasan@users.noreply.github.com>
2022-04-10 11:19:24 +0500

    command api

--- botme-commands-api/controller/utility.py
+++ botme-commands-api/controller/utility.py
@@ -284,7 +284,7 @@ class Utility:
 
     # SUGGESTION RESPONSE
     def suggestionResponse(self):
-        Response = {"ctaId":None,"inputText":{"textValue":self.text,"language":"english","timestamp":self.now},"outputText":{"textValue":""},"context" : {"pageId" : self.pageId,"sectionId":self.sectionId,"parentEntity":{"entityId":"","entityValue":""},"entities" : [{"entityId" : self.call,"entityValue" : self.value,"entitySelected":None,"clickAttribute" : "href, button","keywords" :"product_suggestion"}]}}
+        Response = {"ctaId":None,"inputText":{"textValue":self.text,"language":"english","timestamp":self.now},"outputText":{"textValue":"Here is your Suggestion Response"},"context" : {"pageId" : self.pageId,"sectionId":self.sectionId,"parentEntity":{"entityId":"","entityValue":""},"entities" : [{"entityId" : self.call,"entityValue" : self.value,"entitySelected":None,"clickAttribute" : "href, button","keywords" :"product_suggestion"}]}}
         return Response
         
     # GETTING ENTITYID AND CLICK ATTRIBUTE
