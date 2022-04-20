4e088263487675eca1f10a5c0dbbfcc0daecd73b
HassanSiddiquiMatacs <102812460+HassanSiddiquiMatacs@users.noreply.github.com>
2022-04-20 19:32:43 +0500

    adding device-api work

---
+++ botme-clients-api/src/models/devices.js
@@ -0,0 +1,35 @@
+
+const mongoose = require('mongoose');
+
+const mongodb = require('../utils/mongodb');
+
+var Schema = mongoose.Schema;
+const deviceSchema = new Schema({
+  deviceLabel: {
+    type: String,
+    maxlength: 256,
+    required: true
+  },
+  deviceName: {
+    type: String,
+    maxlength: 256,
+    required: true
+  },
+  deviceType: {
+    type: String,
+    maxlength: 256,
+    required: true
+  },
+  deviceDescription: {
+    type: String,
+    maxlength: 256,
+    required: true
+  }
+//   deviceActive: {
+//     type: Boolean,
+//     required: true
+//   }
+}, {
+  versionKey: false
+});
+module.exports = mongodb.clientsDB.model('devices', deviceSchema);
