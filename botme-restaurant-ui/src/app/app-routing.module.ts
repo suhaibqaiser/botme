dc44a64013698afe29bcf6ba21507247f667c99d
mohsin-matacs <mohsin.ali@d4interactive.io>
2022-06-01 00:20:46 +0500

    working on improvments

--- botme-restaurant-ui/src/app/app-routing.module.ts
+++ botme-restaurant-ui/src/app/app-routing.module.ts
@@ -78,7 +78,7 @@ const routes: Routes = [
     component: SignupComponent
   },
   {
-    path: "product-suggestions",
+    path: "product-search",
     component: SuggestionsComponent,
     canActivate: [AuthenticationGuard]
   },
