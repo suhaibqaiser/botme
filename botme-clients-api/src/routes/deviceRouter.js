0ddfe35078e20ff06f46a9ea7da3f3304bd06fa5
Taha-Hasan <88339456+Taha-Hasan@users.noreply.github.com>
2022-06-02 19:26:16 +0500

    device updated

--- botme-clients-api/src/routes/deviceRouter.js
+++ botme-clients-api/src/routes/deviceRouter.js
@@ -10,9 +10,9 @@ router.get('/list', device_controller.getDeviceList); // GET all Devices details
 
 router.get('/detail', device_controller.getDeviceDetails); // Get device detail on deviceId.
 
-router.put('/register', device_controller.addDevice); // POST request to update device.
+router.put('/register', device_controller.addDevice); // ADD new device.
 
-router.post('/update', device_controller.updateDevice); // DELETE request to delete Device.
+router.post('/update', device_controller.updateDevice);  // UPDATE device detail
 
-router.delete('/remove', device_controller.deleteDevice);
+router.delete('/remove', device_controller.deleteDevice); // DELETE request to delete Device.
 module.exports = router;
