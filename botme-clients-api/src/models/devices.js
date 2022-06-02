17b7a2ea48c87d94cf48a48de068e1b0f754879c
Taha-Hasan <88339456+Taha-Hasan@users.noreply.github.com>
2022-06-02 19:24:26 +0500

    device api endpoints

--- botme-clients-api/src/models/devices.js
+++ botme-clients-api/src/models/devices.js
@@ -4,32 +4,17 @@ const mongoose = require('mongoose');
 const mongodb = require('../utils/mongodb');
 
 var Schema = mongoose.Schema;
-const deviceSchema = new Schema({
-  deviceLabel: {
-    type: String,
-    maxlength: 256,
-    required: true
-  },
-  deviceName: {
-    type: String,
-    maxlength: 256,
-    required: true
-  },
-  deviceType: {
-    type: String,
-    maxlength: 256,
-    required: true
-  },
-  deviceDescription: {
-    type: String,
-    maxlength: 800,
-    required: true
-  },
-  deviceActive: {
-    type: Boolean,
-    required: true
+const deviceSchema = new Schema(
+  {
+    deviceId: {type: String, maxlength: 256, required: true},
+    deviceLabel: {type: String, maxlength: 256, required: true},
+    deviceName: {type: String, maxlength: 256, required: true},
+    deviceType: {type: String, maxlength: 256, required: true},
+    deviceDescription: {type: String, maxlength: 800, required: true},
+    deviceActive: {type: Boolean, required: true}
   }
-}, {
-  versionKey: false
-});
+);
+// }, {
+//   versionKey: false
+// });
 module.exports = mongodb.clientsDB.model('devices', deviceSchema);
