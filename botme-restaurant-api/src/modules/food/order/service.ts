9e2efd7b337597c829586e6bebba4f9636f71d75
Taha-Hasan <88339456+Taha-Hasan@users.noreply.github.com>
2022-06-12 21:34:23 +0500

    search filter admin ui dashboard

--- botme-restaurant-api/src/modules/food/order/service.ts
+++ botme-restaurant-api/src/modules/food/order/service.ts
@@ -23,9 +23,8 @@ export async function getOrder(queryParams: any) {
     return order
 }
 
-export async function queryOrder(queryParams: any, restaurantId: any) {
+export async function queryOrder(queryParams: any) {
     let order: any[] = []
-    queryParams.restaurantId =restaurantId
     try {
         order = await Order.find(queryParams)
     } catch (e) {
